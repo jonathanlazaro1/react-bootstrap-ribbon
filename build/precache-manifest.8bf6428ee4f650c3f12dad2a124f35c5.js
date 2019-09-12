801e0add36dfeb88155a4a9f454385aceb9a7d14
Jonathan Lazzaro <jonathan.lazaro1@gmail.com>
2019-09-12 10:11:19 -0400

    Preparing to publish documentation

---
+++ build/precache-manifest.8bf6428ee4f650c3f12dad2a124f35c5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "63844c7362552e7c5abaabcc69ad77a7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "efe4677812a9c76fac62",
+    "url": "/static/css/main.f8207506.chunk.css"
+  },
+  {
+    "revision": "b8e1ad9eee77cd7f1bb9",
+    "url": "/static/js/2.910fd604.chunk.js"
+  },
+  {
+    "revision": "efe4677812a9c76fac62",
+    "url": "/static/js/main.689d4092.chunk.js"
+  },
+  {
+    "revision": "ef14b63e74754651080e",
+    "url": "/static/js/runtime~main.46ce6b34.js"
+  }
+]);
--- src/index.ts
+++
@@ -1,5 +0,0 @@
-export { default as Ribbon } from "./Ribbon";
-export { default as RibbonGroup } from "./RibbonGroup";
-export { default as RibbonGroupItem } from "./RibbonGroupItem";
-export { default as RibbonButton } from "./RibbonButton";
-export { RibbonButtonIcon, RibbonButtonTitle } from "./RibbonButton";
