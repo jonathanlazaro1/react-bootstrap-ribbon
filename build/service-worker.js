7e4f041effce651bcc0e769a16caf0cd4511e689
Jonathan Lazzaro <jonathan.lazaro1@gmail.com>
2019-09-12 10:26:01 -0400

    Testing docs

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.8bf6428ee4f650c3f12dad2a124f35c5.js"
+  "/precache-manifest.74df2890b1486fdf431962101bb09466.js"
 );
 
 self.addEventListener('message', (event) => {
